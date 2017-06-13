d524886a60f585ce9de436ed3fabee8d1d1be3c4
KeanuNotReeves <keanu.harris.09@gmail.com>
2017-06-13 17:32:49 -0600

    Updated Models

---
+++ Deep Learning.R
@@ -0,0 +1,119 @@
+library(h2o)
+
+#Neural network with H2O package
+#start h20 instance
+localH2O <- h2o.init(ip = "localhost", port = 54321, startH2O = TRUE)
+pathtodata <- paste0(normalizePath("E:/Practicum/Project Data/15FA/"),"/15FA Final Data.xlsx", sheet = "Final 2")
+write.table(x=X15FA, file = pathtodata, row.names = F, col.names = T)
+dat_h2o <- h2o.importFile(path = pathtodata, destination_frame = "X15FA")
+h2o.describe(dat_h2o)
+
+
+#model1 with a deep learning network (three layers of 50 nodes)
+set.seed(54321)
+model1 <- h2o.deeplearning(x=2:18,
+                           y=19,
+                           training_frame = dat_h2o,
+                           activation = "RectifierWithDropout",
+                           input_dropout_ratio = 0.2,
+                           hidden_dropout_ratios = c(0.5,0.5,0.5),
+                           hidden = c(50,50,50),
+                           epochs = 50,
+                           nfolds = 10)
+model1
+plot(model1, main = "Model 1")
+
+#model2 with three layers of 100 nodes
+model2 <- h2o.deeplearning(x=2:18,
+                           y=19,
+                           training_frame = dat_h2o,
+                           activation = "RectifierWithDropout",
+                           input_dropout_ratio = 0.2,
+                           hidden_dropout_ratios = c(0.5,0.5,0.5),
+                           hidden = c(100,100,100),
+                           epochs = 50,
+                           nfolds = 10)
+model2
+plot(model2, main = "Model 2")
+
+#model3 with three layers of 50 nodes and a Tanh activation
+model3 <- h2o.deeplearning(x=2:18,
+                           y=19,
+                           training_frame = dat_h2o,
+                           activation = "TanhWithDropout",
+                           input_dropout_ratio = 0.2,
+                           hidden_dropout_ratios = c(0.5,0.5,0.5),
+                           hidden = c(50,50,50),
+                           epochs = 50,
+                           nfolds = 10)
+model3
+plot(model3, main = "Model 3")
+
+#model4 with three layers of 50 nodes and a Tanh activation, no dropout
+set.seed(14)
+model4 <- h2o.deeplearning(x=2:18,
+                           y=19,
+                           training_frame = dat_h2o,
+                           activation = "Tanh",
+                           input_dropout_ratio = 0.2,
+                           hidden = c(50,50,50),
+                           epochs = 50,
+                           nfolds = 10)
+model4
+plot(model4, main = "Model 4")
+
+#model5 with three layers of 100 nodes and a Tanh activation, no dropout
+set.seed(14)
+model5 <- h2o.deeplearning(x=2:18,
+                           y=19,
+                           training_frame = dat_h2o,
+                           activation = "Tanh",
+                           input_dropout_ratio = 0.2,
+                           hidden = c(100,100,100),
+                           epochs = 50,
+                           nfolds = 10)
+model5
+plot(model5, main = "Model 5")
+
+
+#Neural network with H2O package
+#start h20 instance
+#localH2O <- h2o.init(ip = "localhost", port = 54321, startH2O = TRUE)
+df <- h2o.importFile(paste0(path = normalizePath("E:/Practicum/Project Data/15FA/"),"/15FA Final Data.xlsx", sheet = "Final 2"))
+dim(df)
+df
+splits <- h2o.splitFrame(df, c(0.6,0.2), seed=1234)
+train  <- h2o.assign(splits[[1]], "train.hex") # 60%
+valid  <- h2o.assign(splits[[2]], "valid.hex") # 20%
+test   <- h2o.assign(splits[[3]], "test.hex")  # 20%
+
+
+#model6 with a deep learning network (three layers of 50 nodes)
+model6 <- h2o.deeplearning(x=2:18,
+                           y=19,
+                           set.seed(789),
+                           training_frame = train,
+                           validation_frame = test,
+                           activation = "RectifierWithDropout",
+                           input_dropout_ratio = 0.2,
+                           hidden_dropout_ratios = c(0.5,0.5,0.5),
+                           hidden = c(50,50,50),
+                           epochs = 70,
+                           nfolds = 10,
+                           variable_importances = T)
+model6
+head(as.data.frame(h2o.varimp(model6)))
+
+h2o.performance(model6, train = T)
+h2o.performance(model6, valid = T)
+h2o.performance(model6, newdata = train)
+h2o.performance(model6, newdata = valid)
+h2o.performance(model6, newdata = test)
+
+DLpred <- h2o.predict(model6, test)
+DLpred
+test$Accuracy <- DLpred$predict == test$Enroll
+1-mean(test$Accuracy)
+plot(model6, main = "Model 6")
+
+h2o.logloss(model6, train = train, valid = valid, xval = FALSE)
